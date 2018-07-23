42a1598f364ab944139211c85330f3c6e55687b5
zeal <zhengzx@leadercf.com>
2018-07-23 09:57:34 +0800

    main.js

--- public/admin/js/main.js
+++ public/admin/js/main.js
@@ -1,12 +1,27 @@
 (function ($, AdminLTE) {
     "use strict";
+
+    /**
+     * Get access to plugins
+     */
+
+    $('[data-toggle="control-sidebar"]').controlSidebar()
+    $('[data-toggle="push-menu"]').pushMenu()
+    var $pushMenu = $('[data-toggle="push-menu"]').data('lte.pushmenu')
+    var $controlSidebar = $('[data-toggle="control-sidebar"]').data('lte.controlsidebar')
+    var $layout = $('body').data('lte.layout')
+    $(window).on('load', function() {
+        // Reinitialize variables on load
+        $pushMenu = $('[data-toggle="push-menu"]').data('lte.pushmenu')
+        $controlSidebar = $('[data-toggle="control-sidebar"]').data('lte.controlsidebar')
+        $layout = $('body').data('lte.layout')
+    })
     $("body").toggleClass('fixed');
     setTimeout(function(){
-        AdminLTE.layout.fixSidebar();
-        AdminLTE.pushMenu.expandOnHover();
-        AdminLTE.layout.activate();
-        AdminLTE.controlSidebar._fix($(".control-sidebar-bg"));
-        AdminLTE.controlSidebar._fix($(".control-sidebar"));
+        $layout.fixSidebar();
+        $pushMenu.expandOnHover();
+        $layout.activate();
+        $controlSidebar.fix();
     }, 1000);
     $('body').on('shown.bs.modal', '.modal', function(){
         if ($('body').width() <= 780 && $('body').hasClass('sidebar-open')) {
